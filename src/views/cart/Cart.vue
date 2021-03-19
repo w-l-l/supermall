4446428c948dd49d548a51d6b43f32a17bc01786
w-l-l <1291761009@qq.com>
2021-03-19 21:52:29 +0800

    购物车页面结构搭建

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,12 +1,31 @@
 <template>
-  <h2>购物车</h2>
+  <div class="cart">
+    <nav-bar class="nav-bar">
+      <div slot="center">购物车({{ cartList.length }})</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navBar/NavBar'
+
+import { mapState } from 'vuex'
+
 export default {
-  name: 'Cart'
+  name: 'Cart',
+  components: {
+    NavBar
+  },
+  computed: {
+    ...mapState(['cartList'])
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.cart .nav-bar {
+  background-color: var(--color-tint);
+  color: #fff;
+  font-weight: 700;
+}
 </style>
