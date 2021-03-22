539fa7edce2d7e0b5d30ab76ddaf2fd656adc202
w-l-l <1291761009@qq.com>
2021-03-22 22:09:29 +0800

    fastclick解决移动端点击事件300ms延迟问题

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import store from './store'
 
 import Toast from 'components/common/toast'
 
+// 解决移动端点击事件300ms延迟
+import FastClick from 'fastclick'
+FastClick.attach(document.body)
+
 Vue.config.productionTip = false
 
 Vue.use(Toast)
